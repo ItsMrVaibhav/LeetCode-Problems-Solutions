e9a5eae7343693218b9931bd7683494481480ce9
Vaibhav <59314660+ItsMrVaibhav@users.noreply.github.com>
2020-11-08 03:04:35 +0530

    Update Java-Solution-1.java

--- Problems/Two Sum/Java-Solution-1.java
+++ Problems/Two Sum/Java-Solution-1.java
@@ -5,7 +5,7 @@ class Solution {
         HashMap <Integer, Integer> numsMap = new HashMap <Integer, Integer> (); // Creating a HashMap in order to store and access values faster
         
         for (int i = 0 ; i < nums.length ; i++) {
-            if (numsMap.containsKey(target - nums[i]) && numsMap.get(target - nums[i]) != i) { // Checking if the difference exists inside the HashMao and if it's not the same element
+            if (numsMap.containsKey(target - nums[i]) && numsMap.get(target - nums[i]) != i) { // Checking if the difference exists inside the HashMap and if it's not current number's index
                 return new int [] {i, numsMap.get(target - nums[i])};
             } else {
                 numsMap.put(nums[i], i); // Adding the number as a key and its index as its value to the HashMap

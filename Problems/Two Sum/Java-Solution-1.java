0ed72385474ec12eb70d02d3cd1c57e9a63a0cd0
Vaibhav <59314660+ItsMrVaibhav@users.noreply.github.com>
2020-11-08 03:02:47 +0530

    Create Java-Solution-1.java

---
+++ Problems/Two Sum/Java-Solution-1.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap <Integer, Integer> numsMap = new HashMap <Integer, Integer> (); // Creating a HashMap in order to store and access values faster
+        
+        for (int i = 0 ; i < nums.length ; i++) {
+            if (numsMap.containsKey(target - nums[i]) && numsMap.get(target - nums[i]) != i) { // Checking if the difference exists inside the HashMao and if it's not the same element
+                return new int [] {i, numsMap.get(target - nums[i])};
+            } else {
+                numsMap.put(nums[i], i); // Adding the number as a key and its index as its value to the HashMap
+            }
+        }
+        
+        return null;
+    }
+}

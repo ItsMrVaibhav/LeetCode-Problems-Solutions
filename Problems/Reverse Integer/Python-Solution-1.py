617c509a2c0ca4b2c2fe6c0dcb548480b83a36f4
Vaibhav <59314660+ItsMrVaibhav@users.noreply.github.com>
2020-11-08 21:19:46 +0530

    Update Python-Solution-1.py

--- Problems/Reverse Integer/Python-Solution-1.py
+++ Problems/Reverse Integer/Python-Solution-1.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def reverse(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
+class Solution:
+    def reverse(self, x: int) -> int:
         number = int(str(x)[::-1]) if x >= 0 else -int(str(abs(x))[::-1]) # Considering absolute value when the number is negative, and making it negative after reversing
         
         return number if (-2 ** 31) <= number <= (2**31 - 1) else 0 # Return the reversed number if it lies within the limit. Otherwise, zero.
